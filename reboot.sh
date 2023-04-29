ca1f152eae311dd4d98bdde98c72ae30f9bf885f
邱开伟 <sucreqiu@gmail.com>
2023-04-29 20:37:13 +0800

    在重启脚本中添加了重新获取静态文件的命令

--- reboot.sh
+++ reboot.sh
@@ -16,6 +16,11 @@ echo -e '\n----------check if the kill action is correct----------'
 
 /envs/my_application/bin/uwsgi  --ini uwsgi_my_application.ini &  >/dev/null
 
+echo -e '\n--------------获取静态文件，完全覆盖原来的文件--------------'
+sleep 1
+
+python3 manage.py collectstatic --noinput
+
 echo -e '\n\033[42;1m----------------------started...----------------------\033[0m'
 sleep 1
 
