9ea326e0ecce679a177fc30a13b7fcf47601f25f
邱开伟 <sucreqiu@gmail.com>
2023-06-23 17:32:50 +0800

    将chunk转换成utf-8-6

--- ai_apps/views/translator.py
+++ ai_apps/views/translator.py
@@ -92,7 +92,8 @@ def generate_stream_data(text):
 
     for chunk in chunks:
 
-        result = chunk.choices[0].get("delta", {}).get("content")
+        # result = chunk.choices[0].get("delta", {}).get("content")
+        result = chunk.choices[0]['delta']['content']
         # 如果有需要，可以打印出来看看
         print(chunk)
         print(result)
