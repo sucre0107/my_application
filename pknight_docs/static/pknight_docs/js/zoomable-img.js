437e98c78728ebf6da61bf79834dc93cbef78012
邱开伟 <sucreqiu@gmail.com>
2023-06-25 21:44:38 +0800

    usbelf 完善

--- pknight_docs/static/pknight_docs/js/zoomable-img.js
+++ pknight_docs/static/pknight_docs/js/zoomable-img.js
@@ -4,7 +4,8 @@ zoomableImages.forEach((image) => {
         const fullscreenImage = document.createElement('img');
         fullscreenImage.src = image.src;
         fullscreenImage.alt = image.alt;
-        fullscreenImage.classList.add('fixed', 'top-0', 'left-0', 'w-full', 'h-full', 'z-50', 'object-contain', 'cursor-zoom-out');
+        // 添加类，object-contain 保持图片比例，cursor-zoom-out 鼠标移入时显示放大图标，backdrop-blur-lg 背景模糊
+        fullscreenImage.classList.add('fixed', 'top-0', 'left-0', 'w-full', 'h-full', 'z-50', 'object-contain', 'cursor-zoom-out', 'backdrop-blur-lg');
         document.body.appendChild(fullscreenImage);
         document.body.classList.add('overflow-hidden');
         fullscreenImage.addEventListener('click', () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
       './pknight_docs/templates/**/*.html',
       './pknight_docs/templates/*.html',
       './ai_apps/forms.py',
+      // 只要是用到 tailwind 的地方都要加入
+      './pknight_docs/static/pknight_docs/js/zoomable-img.js',
 
   ],
   theme: {
