eabeffc3b51206da26bd57ef486b61ab45da7efc
邱开伟 <sucreqiu@gmail.com>
2023-06-27 17:40:00 +0800

    增加ai客服

--- ai_apps/forms.py
+++ ai_apps/forms.py
@@ -34,7 +34,28 @@ class EmailWriterForm(forms.Form):
                                     widget=forms.TextInput(attrs={'class': 'p-2.5 w-full text-xl text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-blue-500'}))
     include_info = forms.CharField(label='我想在回复中包含的信息',
                                     initial="",
-                                    widget=forms.TextInput(attrs={'class': 'p-2.5 w-full text-xl text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-blue-500'}))
+                                    widget=forms.Textarea(attrs={'class': 'p-2.5 w-full h-20 text-xl text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-blue-500'}))
     # 用来接收翻译好的文本
     my_response_email = forms.CharField(label='我的回复',
+                                  widget=forms.Textarea(attrs={'class': 'cus-textarea'}))
+
+
+class CustomerServiceAssistantForm(forms.Form):
+    # 用来接收用户输入的文本
+    customer_message = forms.CharField(label='顾客的信息',
+                           widget=forms.Textarea(attrs={'class': 'cus-textarea'}))
+
+    #language = forms.ChoiceField(label='翻译成',
+                                 # required=True,
+                                 # choices=[('zh', '中文'), ('en', '英文'), ('ru', '俄文'), ('ko', '韩文')],
+                                 # widget=forms.Select(attrs={'class': 'select select-bordered w-32'}))
+    # 我对于ai助手帮我写的邮件的额外要求
+    my_extra_requirement = forms.CharField(label='我回复时要注意的事项',
+                                    initial="be polite",
+                                    widget=forms.TextInput(attrs={'class': 'p-2.5 w-full text-xl text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-blue-500'}))
+    include_info = forms.CharField(label='我想在回复中包含的信息',
+                                    initial="",
+                                    widget=forms.Textarea(attrs={'class': 'p-2.5 w-full h-20 text-xl text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-blue-500'}))
+    # 用来接收翻译好的文本
+    my_reply = forms.CharField(label='我的回复',
                                   widget=forms.Textarea(attrs={'class': 'cus-textarea'}))
--- ai_apps/urls.py
+++ ai_apps/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.static import static
 from django.urls import path
 from ai_apps import views
-from ai_apps.views import translator,index,email
+from ai_apps.views import translator, index, email, customer_service_assistant
 from django.conf import settings
 
 urlpatterns = [
@@ -9,5 +9,7 @@ urlpatterns = [
                   path("trans/", translator.translator, name="translator"),
                   path("trans/translate/", translator.pack_trans_stream, name="pack_trans_stream"),
                   path("email/", email.email_writer, name="email_writer"),
-                  path("email/generate/", email.pack_email_stream, name="pack_email_stream")
+                  path("email/generate/", email.pack_email_stream, name="pack_email_stream"),
+                  path("csa/", customer_service_assistant.customer_service_assistant, name="csa"),
+                  path("csa/generate/", customer_service_assistant.pack_reply_stream, name="pack_reply_stream"),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
