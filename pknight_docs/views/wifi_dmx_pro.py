e9f802f9820675bd895a2068f1175781f3b6b5c5
邱开伟 <sucreqiu@gmail.com>
2024-04-01 13:50:13 +0800

    添加了一个wifi dmx pro 的下载页面

---
+++ pknight_docs/views/wifi_dmx_pro.py
@@ -0,0 +1,11 @@
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+def wifi_dmx_pro_apps(request):
+    path_list = ''
+    if request.path == "/pknight_docs/wifidmxpro/apps/android/":
+        return HttpResponse(render(request, '• wifi_dmx_pro.html'))
+    else:
+        print(request.path)
+        return HttpResponse("Invalid app type.", status=400)
