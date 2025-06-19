a6ea84ad74bc6afb86ce179afe3f8ebf0fcd78ad
邱开伟 <sucreqiu@gmail.com>
2025-06-19 14:08:28 +0800

    first try 程序后台运行

--- reboot.sh
+++ reboot.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env bash
 
 echo -e "\033[34m--------------------wsgi process--------------------\033[0m"
-
-ps -ef|grep uwsgi_params.ini | grep -v grep
+ps -ef | grep uwsgi_params.ini | grep -v grep
 
 sleep 0.5
 
 echo -e '\n--------------------going to close--------------------'
-
-ps -ef |grep uwsgi_params.ini | grep -v grep | awk '{print $2}' | xargs kill -9
-
+pkill -f uwsgi_params.ini
 sleep 0.5
 
 echo -e '\n----------check if the kill action is correct----------'
-
-/envs/my_application/bin/uwsgi  --ini uwsgi_params.ini &  >/dev/null
+ps -ef | grep uwsgi_params.ini | grep -v grep
 
 echo -e '\n--------------获取静态文件，完全覆盖原来的文件--------------'
-sleep 1
+python3 manage.py collectstatic --noinput || echo "collectstatic failed"
 
-python3 manage.py collectstatic --noinput
+echo -e '\n\033[42;1m----------------------starting uwsgi...----------------------\033[0m'
+/envs/my_application/bin/uwsgi --ini uwsgi_params.ini
 
-echo -e '\n\033[42;1m----------------------started...----------------------\033[0m'
-sleep 1
-
-ps -ef |grep uwsgi_params.ini | grep -v grep
+echo -e '\n----------------------current uwsgi status----------------------'
+ps -ef | grep uwsgi_params.ini | grep -v grep
